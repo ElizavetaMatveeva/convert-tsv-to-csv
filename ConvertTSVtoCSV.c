1d66810e3a742a7fc4afcc8d4bae8cd0ffe0f971
ElizavetaMatveeva <36740409+ElizavetaMatveeva@users.noreply.github.com>
2019-08-29 14:32:15 -0500

    Add files via upload

---
+++ ConvertTSVtoCSV.c
@@ -0,0 +1,28 @@
+/*
+ITMO University homework, 2015
+Task: convert TSV file named "ListOfStudents.txt" to CSV file 
+*/
+
+#include <stdio.h>
+
+void TSVtoCSV(FILE* f, FILE* f1)
+{
+	char c;
+	while (c!= EOF)
+	{
+		c=fgetc(f);
+		if (c=='\t') 
+			c=','; // Заменяем табуляцию на запятые
+		putc(c, f1); 
+	}
+}
+
+int main()
+{
+	FILE* f=fopen("ListOfStudents.txt", "r");
+	FILE* f1=fopen("StudentsCSV.txt", "w");
+	TSVtoCSV(f, f1); // Применяем функцию для создания или перезаписи нового файла
+	fclose(f);
+	fclose(f1);
+	return 0;
+}
